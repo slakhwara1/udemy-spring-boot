16af28f3a5c13207768f42f56e358c15458a577c
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-20 17:04:17 +0530

    AOP @after-throwing advice sniffing the exception
    AOP @after-throwing advice sniffing the exception

---
+++ spring-demo-aop-z-after-throwing/src/com/springPractice/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,81 @@
+package com.springPractice.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.springPractice.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+
+	@AfterThrowing(
+			pointcut = "execution(* com.springPractice.aopdemo.dao.AccountDAO.findAccounts(..))",
+			throwing = "exc")
+	public void afterThrowingFindAccountsAdvice(JoinPoint joinPoint, Throwable exc) {
+		
+		String method = joinPoint.getSignature().toShortString();
+		
+		System.out.println("\n=====>>>>> Executing @AfterThrowing on method : " + method);
+		
+		System.out.println("\n=====>>>>> The exception is : " + exc);		
+		
+	}
+	
+
+	@AfterReturning(
+			pointcut = "execution(* com.springPractice.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning = "result")
+	public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result) {
+		
+		String method = joinPoint.getSignature().toShortString();
+		
+		System.out.println("\n=====>>>>> Executing @AfterReturning on method : " + method);
+		
+		System.out.println("\n=====>>>>> Executing @AfterReturning on method : " + result);
+
+		convertAccountNamesToUpperCase(result);
+
+	}
+	
+
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+		
+		result.forEach(account -> account.setName(account.getName().toUpperCase()));
+		
+	}
+
+
+	@Before("com.springPractice.aopdemo.aspect.SpringAppAopExpressions.forDaoPackageNoGetterAndSetter()")
+	public void beforeAddAccount(JoinPoint joinPoint) {
+
+		System.out.println("\n\n ===>>>>>>> Ecxecuting @Before advice on ");
+		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+		System.out.println("Method: " + methodSignature);
+
+		Object[] args = joinPoint.getArgs();
+
+		for (Object tempArg : args) {
+			System.out.println(tempArg);
+
+			if (tempArg instanceof Account) {
+				Account account = (Account) tempArg;
+				System.out.println("Account name : " + account.getName());
+				System.out.println("Account level : " + account.getLevel());
+			}
+		}
+
+	}
+
+}
---
+++ spring-demo-aop-z-after-throwing/src/com/springPractice/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,64 @@
+package com.springPractice.aopdemo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.springPractice.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+
+	private String name;
+	private String serviceCode;
+
+	public List<Account> findAccounts(boolean tripWire) {
+		
+		if(tripWire) {
+			throw new RuntimeException("No soup for you");
+		}
+		
+		List<Account> accounts = new ArrayList<>();
+
+		Account account1 = new Account("John", "Silver");
+		Account account2 = new Account("Madhu", "Platinum");
+		Account account3 = new Account("Luca", "Gold");
+
+		accounts.add(account1);
+		accounts.add(account2);
+		accounts.add(account3);
+
+		return accounts;
+	}
+
+	public void addAccount(Account account, boolean vipFlag) {
+		System.out.println(getClass() + " Doing my DB work: adding an account");
+	}
+
+	public boolean doWork() {
+		System.out.println(getClass() + " : doWork()");
+		return true;
+	}
+
+	public String getName() {
+		System.out.println(getClass() + " in getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + " in setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + " in getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + " in setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
+
+}
