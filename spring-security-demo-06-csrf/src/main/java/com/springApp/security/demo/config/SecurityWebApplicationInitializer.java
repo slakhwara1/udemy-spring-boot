1485c85ba82063608a0751f39ae5ade86a44f192
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-21 18:43:32 +0530

    Have to add manual CSRF token in the fancy-login-form

---
+++ spring-security-demo-06-csrf/src/main/java/com/springApp/security/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.springApp.security.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ spring-security-demo-06-csrf/src/main/java/com/springApp/security/demo/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.springApp.security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+
+//		return "plain-login";
+		return "fancy-login";
+
+	}
+
+}
