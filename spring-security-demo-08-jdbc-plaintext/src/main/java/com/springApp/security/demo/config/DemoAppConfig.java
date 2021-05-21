782944ccdc975a8a5d08bb70ea8c317482024d31
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-21 22:29:34 +0530

    Spring security using JDBC authentication

--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/springApp/security/demo/config/DemoAppConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/springApp/security/demo/config/DemoAppConfig.java
@@ -1,16 +1,32 @@
 package com.springApp.security.demo.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.springApp.security.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+	@Autowired
+	private Environment env;
+
+	private Logger logger = Logger.getLogger(getClass().getName());
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -19,7 +35,41 @@ public class DemoAppConfig {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+
+	@Bean
+	public DataSource securityDataSource() {
+
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+			
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+
+		logger.info("\n\n =====>>>>> jdbc.url = " + env.getProperty("jdbc.url"));
+		logger.info(" =====>>>>> jdbc.user = " + env.getProperty("jdbc.user"));
+		logger.info(" =====>>>>> jdbc.password = " + env.getProperty("jdbc.password"));
+		
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		securityDataSource.setInitialPoolSize(this.getIntProperty("connection.pool.initialPoolSize"));
+		securityDataSource.setMinPoolSize(this.getIntProperty("connection.pool.minPoolSize"));
+		securityDataSource.setMaxPoolSize(this.getIntProperty("connection.pool.maxPoolSize"));
+		securityDataSource.setMaxIdleTime(this.getIntProperty("connection.pool.maxIdleTime"));
+		
+		return securityDataSource;
+	}
 	
-	
+	private int getIntProperty(String propName) {
+		String propVal = this.env.getProperty(propName);
+		
+		int intPropVal = Integer.parseInt(propVal);
+		
+		return intPropVal;
+	}
 
 }
--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/springApp/security/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/springApp/security/demo/config/DemoSecurityConfig.java
@@ -1,39 +1,36 @@
 package com.springApp.security.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		auth.inMemoryAuthentication().withUser(users.username("John").password("test123").roles("EMPLOYEE"))
-				.withUser(users.username("Mary").password("test123").roles("EMPLOYEE" , "MANAGER"))
-				.withUser(users.username("Susan").password("test123").roles("EMPLOYEE" , "ADMIN"));
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests()
-		.antMatchers("/").hasRole("EMPLOYEE")
-		.antMatchers("/leaders/**").hasRole("MANAGER")
-		.antMatchers("/systems/**").hasRole("ADMIN")
+		http.authorizeRequests().antMatchers("/").hasRole("EMPLOYEE").antMatchers("/leaders/**").hasRole("MANAGER")
+				.antMatchers("/systems/**").hasRole("ADMIN")
 //		.anyRequest().authenticated()
-		.and().formLogin().loginPage("/showMyLoginPage")
-				.loginProcessingUrl("/authenticateTheUser").permitAll().and().logout().permitAll()
-				.and().exceptionHandling().accessDeniedPage("/access-denied");
+				.and().formLogin().loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll()
+				.and().logout().permitAll().and().exceptionHandling().accessDeniedPage("/access-denied");
 
 	}
 
