f2a8a45428400f04b14815d8ee3bc91fc82c838a
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-22 10:10:01 +0530

    Spring REST getting students with a given id

--- spring-rest-demo/src/main/java/com/springRest/demo/entity/Student.java
+++ spring-rest-demo/src/main/java/com/springRest/demo/entity/Student.java
@@ -2,6 +2,8 @@ package com.springRest.demo.entity;
 
 public class Student {
 
+	private int id;
+
 	private String firstName;
 	private String lastName;
 
@@ -9,9 +11,18 @@ public class Student {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Student(String firstName, String lastName) {
+	public Student(int id, String firstName, String lastName) {
+		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getFirstName() {
--- spring-rest-demo/src/main/java/com/springRest/demo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/springRest/demo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.springRest.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +16,37 @@ import com.springRest.demo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
+	private List<Student> students;
+
+	@PostConstruct
+	public void loadData() {
+		this.students = this.getLocalStudentList();
+	}
+
 	@GetMapping("/students")
 	public List<Student> getStudents() {
+
+		return this.students;
+	}
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return this.students.get(studentId);
+	}
+
+	private List<Student> getLocalStudentList() {
 		List<Student> students = new ArrayList<Student>();
 
-		Student student1 = new Student("John", "Doe");
-		Student student2 = new Student("Mary", "Jane");
-		Student student3 = new Student("Susan", "Phillips");
+		Student student1 = new Student(1, "John", "Doe");
+		Student student2 = new Student(2, "Mary", "Jane");
+		Student student3 = new Student(3, "Susan", "Phillips");
 
 		students.add(student1);
 		students.add(student2);
 		students.add(student3);
 
 		return students;
+
 	}
 
 }
