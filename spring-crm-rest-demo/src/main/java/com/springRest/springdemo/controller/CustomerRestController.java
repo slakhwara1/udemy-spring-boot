6145ad83954fc0e86e199807866f142597e17453
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-23 20:53:20 +0530

    Added endpoint for deleting customers

--- spring-crm-rest-demo/src/main/java/com/springRest/springdemo/controller/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/springRest/springdemo/controller/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.springRest.springdemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,21 @@ public class CustomerRestController {
 		this.customerService.saveCustomer(customer);
 
 		return customer;
+	}
+
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+
+		Customer customer = this.customerService.getCustomer(customerId);
+
+		if (customer == null) {
+			throw new CustomerNotFoundException("Customer with id - " + customerId + " not found");
+		}
+
+		this.customerService.deleteCustomer(customerId);
+
+		return "Deleted Customer with id - " + customerId;
+
 	}
 
 }
