58afe63a62cd2007bbaaff3e659f016a1ee53185
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-23 18:14:04 +0530

    Initial spring-crm-rest project setup
    Initial spring-crm-rest project setup

---
+++ spring-crm-rest-demo/src/main/java/com/springRest/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.springRest.springdemo.service;
+
+import java.util.List;
+
+import com.springRest.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
