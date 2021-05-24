31c2ee65e1f26d632bfd142424ddb925192668f1
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-24 10:28:49 +0530

    Added functionality for accessing spring boot actuator endpoints

---
+++ spring-boot-03-actuator-demo/src/test/java/com/springBoot/myApp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.myApp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
