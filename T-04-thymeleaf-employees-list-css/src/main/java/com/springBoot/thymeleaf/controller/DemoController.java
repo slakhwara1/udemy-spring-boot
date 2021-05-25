327501308eccf93e2a1d07d2210580ad816a8b8e
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-25 19:56:10 +0530

    Spring boot thymeleaf Added bootstrap CDN to the templates

---
+++ T-04-thymeleaf-employees-list-css/src/main/java/com/springBoot/thymeleaf/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.springBoot.thymeleaf.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	@GetMapping("/hello")
+	public String sayHello(Model model) {
+		
+		model.addAttribute("theDate", LocalDateTime.now());
+		
+		return "helloworld";
+	}
+
+}
