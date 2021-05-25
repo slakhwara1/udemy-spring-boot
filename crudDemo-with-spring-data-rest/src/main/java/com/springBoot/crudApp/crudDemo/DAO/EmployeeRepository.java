4fb6a7d2b791ff60dba0cdf72da6bd2e08b27f9e
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-25 12:29:54 +0530

    Spring Data REST Configuration, Pagination and sorting

--- crudDemo-with-spring-data-rest/src/main/java/com/springBoot/crudApp/crudDemo/DAO/EmployeeRepository.java
+++ crudDemo-with-spring-data-rest/src/main/java/com/springBoot/crudApp/crudDemo/DAO/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.springBoot.crudApp.crudDemo.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springBoot.crudApp.crudDemo.entity.Employee;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
