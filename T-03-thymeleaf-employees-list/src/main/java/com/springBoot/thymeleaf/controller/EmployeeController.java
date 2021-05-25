5c1cb52fa1a4af5dda253e7a32dbd769d4570bab
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-25 19:30:20 +0530

    Spring boot thymeleaf template building database tables

---
+++ T-03-thymeleaf-employees-list/src/main/java/com/springBoot/thymeleaf/controller/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.springBoot.thymeleaf.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springBoot.thymeleaf.model.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private List<Employee> employees;
+	
+	@PostConstruct
+	private void loadData () {
+
+		Employee employee1 = new Employee("Leslie", "Andrews", "leslie@gmail.com");
+		Employee employee2 = new Employee("Emma", "Baumgarten", "emma@gmail.com");
+		Employee employee3 = new Employee("Avani", "Gupta", "avani@gmail.com");
+		
+		this.employees = new ArrayList<Employee>();
+		this.employees.add(employee1);
+		this.employees.add(employee2);
+		this.employees.add(employee3);
+		
+	}
+
+	
+	@GetMapping("/list")
+	public String listEmployees(Model model) {
+		
+		model.addAttribute("employees", this.employees);
+		
+		return "list-employees";
+	}
+}
