115dfa2ab539ab422daba528b2f0b34ebfe6e2cd
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-05-25 11:28:23 +0530

    Spring-boot using Spring Data JPA for performing CRUD operations

---
+++ crudDemo-with-spring-data-jpa/src/main/java/com/springBoot/crudApp/crudDemo/CrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.crudApp.crudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoApplication.class, args);
+	}
+
+}
---
+++ crudDemo-with-spring-data-jpa/src/test/java/com/springBoot/crudApp/crudDemo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.crudApp.crudDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
